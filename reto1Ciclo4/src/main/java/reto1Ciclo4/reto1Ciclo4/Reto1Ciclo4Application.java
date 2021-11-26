af9ef01695d0209cd5d1f43d15b08fdd21d1a0ac
Leidy Cruz <leidy.cruz.mt@correo.usa.edu.co>
2021-11-26 18:06:24 -0500

    Reto 1 Ciclo 4

---
+++ reto1Ciclo4/src/main/java/reto1Ciclo4/reto1Ciclo4/Reto1Ciclo4Application.java
@@ -0,0 +1,13 @@
+package reto1Ciclo4.reto1Ciclo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1Ciclo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Ciclo4Application.class, args);
+	}
+
+}
---
+++ reto1Ciclo4/src/test/java/reto1Ciclo4/reto1Ciclo4/Reto1Ciclo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package reto1Ciclo4.reto1Ciclo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1Ciclo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
